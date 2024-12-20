e915490bb3f01e38fda26cd421ea67222e4480b8
Priyanshi <guptapriyanshi6232@gmail.com>
2024-12-20 16:10:50 +0530

    ttt

---
+++ src/Main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import tictactoe1.TicTacToe1;
+
+public class Main 
+{
+    public static void main(String args[])
+    {
+        
+        TicTacToe1 tictactoe = new TicTacToe1();
+    }        
+     
+}
